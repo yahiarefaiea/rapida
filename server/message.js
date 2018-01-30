c6600623b4f8d90d2e26414f2da12b046dbd2a9e
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-01-30 14:35:11 +0200

    codes improvement

--- server/message.js
+++ server/message.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk';
 
 /* eslint-disable no-console */
-console.log(chalk.blue('Application started in development mode..'));
+console.log(chalk.cyan('Application started in development mode..'));
