c926b099cedfc58207387ff32c28bf0b28fccead
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-05 08:56:02 +0200

    api v1 routes updated

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -2,14 +2,13 @@ import express from 'express'
 
 // v1 imports
 import book from './book/routes'
-import bookModel from './book/model'
 
 // export function
 const v1 = function() {
   const v1Router = express.Router()
 
   // v1 middlewares
-  v1Router.use('/book', book(bookModel))
+  v1Router.use('/book', book())
 
   return v1Router
 }
