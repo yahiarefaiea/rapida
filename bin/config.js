c8d5f06be1f8797ee09e3c73ef43ab37c310ac69
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-07 08:44:32 +0200

    bin/config update

--- bin/config.js
+++ bin/config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  env: process.env.NODE_ENV,
   localhost: 'local.dev',
   domain: 'nuotron.co',
-  database: 'library',
+  db: 'library',
   port: function() {
     const val = process.env.PORT || '3000'
     const port = parseInt(val, 10)
@@ -9,5 +10,17 @@ module.exports = {
     if(isNaN(port)) return val
     if(port >= 0) return port
     return false
+  },
+  url: function() {
+    if(this.env === 'development')
+      return `${this.localhost}:${this.port()}`)
+    else if(this.env === 'production')
+      return this.domain
+  },
+  database: function() {
+    if(this.env === 'development')
+      return `mongodb://${this.localhost}/${this.db}`)
+    else if(this.env === 'production')
+      return `mongodb://${this.domain}/${this.db}`)
   }
 }
