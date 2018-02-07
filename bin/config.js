5389a6104901616d45634ca3678e4a19f9c7fd7e
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-07 05:44:36 +0200

    config update

--- bin/config.js
+++ bin/config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  port: function normalizePort(normalizePort(process.env.PORT || '3000')) {
+  localhost: 'local.dev',
+  domain: 'nuotron.co',
+  database: 'library',
+  port: function() {
+    const val = process.env.PORT || '3000'
     const port = parseInt(val, 10)
+
     if(isNaN(port)) return val
     if(port >= 0) return port
     return false
-  },
-  localhost: 'local.dev',
-  domain: 'nuotron.co',
-  database: 'library'
+  }
 }
