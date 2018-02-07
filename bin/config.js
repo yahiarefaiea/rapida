ef88e9774a01b730c6c563a597dfe2b9ab4f69d8
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-07 05:30:59 +0200

    fixed some problems

--- bin/config.js
+++ bin/config.js
@@ -1,23 +1,11 @@
 module.exports = {
-  port: normalizePort(process.env.PORT || '3000'),
+  port: function normalizePort(normalizePort(process.env.PORT || '3000')) {
+    const port = parseInt(val, 10)
+    if(isNaN(port)) return val
+    if(port >= 0) return port
+    return false
+  },
   localhost: 'local.dev',
   domain: 'nuotron.co',
   database: 'library'
-}
-
-// normalize a port into a number, string, or false
-function normalizePort(val) {
-  const port = parseInt(val, 10)
-
-  if(isNaN(port)) {
-    // named pipe
-    return val
-  }
-
-  if(port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
 }
