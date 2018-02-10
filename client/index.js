40b794c9259ec7c0c459c8f9d1f36f46f9e69b55
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-10 14:08:28 +0200

    client layout added

--- client/index.js
+++ client/index.js
@@ -7,15 +7,20 @@ const router = express.Router()
 module.exports = function() {
   return router
     // router middlewares
-    .use(express.static('client/public'))
+    .use(express.static('public'))
 
     // error handler
     .use(function(req, res, next) {
       next(new Errors.NotFound())
     })
     .use(function(err, req, res, next) {
+      const data = {
+        status: err.statusCode,
+        message: err.message,
+        title: `${this.status} ${this.message}`
+      }
       if(err && err.statusCode)
-        res.status(err.statusCode).render('client/error', {status: err.statusCode, message: err.message})
+        res.status(err.statusCode).render('client/error', data)
       else next(err)
     })
 }
