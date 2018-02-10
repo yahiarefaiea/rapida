189bdb7f07329bb8e0753c1b0b2a41f341d2ae72
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-10 07:48:20 +0200

    error handler in client

--- client/index.js
+++ client/index.js
@@ -5,7 +5,14 @@ const router = express.Router()
 // export function
 module.exports = function() {
   return router
-    .get('/', function(req, res) {
-      res.send('You\'re in client from client/index.js !')
+
+    // error handler
+    .use(function(req, res, next) {
+      next(new Errors.NotFound())
+    })
+    .use(function(err, req, res, next) {
+      if(err && err.statusCode) {
+        res.status(err.statusCode).send(`${err.statusCode} ${err.message}`)
+      } else next(err)
     })
 }
