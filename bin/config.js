7c439bc041ccb4a380eea17567028afa95b2e408
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-12 12:59:49 +0200

    DB_NAME update in bin/config.js

--- bin/config.js
+++ bin/config.js
@@ -1,3 +1,6 @@
+import pkg from '../package.json'
+
+// export function
 module.exports = {
   env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || 'local.dev',
@@ -5,7 +8,7 @@ module.exports = {
   db: {
     host: process.env.DB_HOST || 'local.dev',
     port: process.env.DB_PORT || '27017',
-    name: process.env.DB_NAME || process.argv0
+    name: process.env.DB_NAME || pkg.name
   },
   database: function() {
     return `mongodb://${this.db.host}:${this.db.port}/${this.db.name}`
