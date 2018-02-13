a49a0e5ad1bfe21abb34dbb430dd5f5af43b601f
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-13 06:49:20 +0200

    book routes testing

--- api/v1/index.js
+++ api/v1/index.js
@@ -3,11 +3,11 @@ import express from 'express'
 const router = express.Router()
 
 // router imports
-// import book from './book'
+import book from './book'
 
 // export function
 module.exports = function() {
   return router
   // router middlewares
-  // .use(subdomain('book', book()))
+  .use('/book', book())
 }
