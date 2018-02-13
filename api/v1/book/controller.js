46714b8cd8b6ad3fd81342ebed8b81a3cfa5d6ea
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-13 16:05:29 +0200

    built the skeleton for the book controller

--- api/v1/book/controller.js
+++ api/v1/book/controller.js
@@ -1,5 +1,35 @@
 import Book from './model'
+import errHandle from 'rapid-error-handler'
 
 // export function
 module.exports = {
+  // get all items
+  getAll: function(req, res, next) {
+  },
+
+  // post new item
+  post: function(req, res, next) {
+  },
+
+  // find one item
+  find: function(req, res, next) {
+  },
+
+  // get one item
+  get: function(req, res, next) {
+  },
+
+  // update an item
+  patch: function(req, res, next) {
+  },
+
+  // delete an item
+  delete: function(req, res, next) {
+  }
 }
+
+// handler function
+// function handler(err, req, res, next, status, msg) {
+//   if(err) next(new Errors.BadRequest(err))
+//   else res.status(status).send(msg)
+// }
