85aaf14c02ddbcc4e546f133071a90b9229f6dd2
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-14 10:16:56 +0200

    resolve controller with the new handler function

--- api/v1/book/controller.js
+++ api/v1/book/controller.js
@@ -5,18 +5,43 @@ import Book from './model'
 module.exports = {
   // get all items
   getAll: function(req, res, next) {
+    const query = {}
+    if(req.query.read)
+      query.read = req.query.read
+
+    Book.find(query, function(err, books) {
+      handler(err, next, function() {
+        res.send(books)
+      })
+    })
   },
 
   // post new item
   post: function(req, res, next) {
+    const book = new Book(req.body)
+
+    book.save(function(err) {
+      handler(err, next, function() {
+        res.status(201).send(book)
+      })
+    })
   },
 
   // find one item
   find: function(req, res, next) {
+    Book.findById(req.params.id, function(err, book) {
+      handler(err, next, function() {
+        if(book) {
+          req.book = book
+          next()
+        } else next(new errHandle.NotFound('Book not found'))
+      })
+    })
   },
 
   // get one item
   get: function(req, res, next) {
+    res.send(req.book)
   },
 
   // update an item
