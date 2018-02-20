9cac4758da912be22c7747fbcd3a9ec848c9cc3b
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-20 13:04:50 +0200

    regex update in author validation

--- api/v1/book/model.js
+++ api/v1/book/model.js
@@ -26,7 +26,7 @@ module.exports = mongoose.model('Book', new mongoose.Schema({
       required: [true, 'email must be provided'],
       validate: {
         validator: function(email) {
-          regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          const regex = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
           return regex.test(email)
         },
         message: 'this is not a valid email'
