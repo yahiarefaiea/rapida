9359a278a4233846e959a93f64f85c4467b3bd71
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-02-20 12:15:04 +0200

    book model schema update

--- api/v1/book/model.js
+++ api/v1/book/model.js
@@ -4,11 +4,34 @@ import mongoose from 'mongoose'
 module.exports = mongoose.model('Book', new mongoose.Schema({
   title: {
     type: String,
+    maxlength: [48, 'title must be less than 48 characters'],
     required: [true, 'title must be provided']
   },
   author: {
-    type: String,
-    required: [true, 'author must be provided']
+    name: {
+      first: {
+        type: String,
+        maxlength: [18, 'first name must be less than 18 characters'],
+        required: [true, 'first name must be provided']
+      },
+      last: {
+        type: String,
+        maxlength: [18, 'last name must be less than 18 characters'],
+        required: [true, 'last name must be provided']
+      }
+    },
+    email: {
+      type: String,
+      unique: [true, 'email already exists'],
+      required: [true, 'email must be provided'],
+      validate: {
+        validator: function(email) {
+          regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return regex.test(email)
+        },
+        message: 'this is not a valid email'
+      }
+    }
   },
   read: {
     type: Boolean,
