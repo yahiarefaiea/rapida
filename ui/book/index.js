d5ea53424ab6c1f1487002483d7240e37808344f
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-11-26 20:50:17 +0200

    added book feature

---
+++ ui/book/index.js
@@ -0,0 +1,4 @@
+const name = 'Client'
+
+// eslint-disable-next-line no-console
+console.log(`Hello from the ${name} :(`)
---
+++ ui/book/index.test.js
@@ -0,0 +1,14 @@
+import {expect} from 'chai'
+import jsdom from 'jsdom'
+import pug from 'pug'
+const {JSDOM} = jsdom
+
+describe('index.pug', function() {
+  it('should say hello', function(done) {
+    const index = new JSDOM(pug.renderFile('./client/source/index.pug'))
+    const h1 = index.window.document.querySelector('h1').textContent
+    expect(h1).to.equal('this is heading')
+    done()
+    window.close()
+  })
+})
