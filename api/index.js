598b981ba31d5eb3122b51dffca69051c9661209
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-11-26 13:54:20 +0200

    update files tree

--- api/index.js
+++ api/index.js
@@ -1,25 +1,41 @@
-// api router
-import subdomain from 'express-subdomain'
+// api v1 router
 import express from 'express'
-import errHandle from 'rapid-error-handler'
 const router = express.Router()
 
 // router imports
-import v1 from './v1'
+import book from './book'
 
 // export function
 module.exports = function() {
   return router
     // router middlewares
-    .use(subdomain('v1', v1()))
-
-    // error handler
-    .use(function(req, res, next) {
-      next(new errHandle.Forbidden())
-    })
-    .use(function(err, req, res, next) {
-      if(err && err.statusCode)
-        res.status(err.statusCode).send({status: err.statusCode, message: err.message})
-      else next(err)
-    })
+    .use('/book', book())
 }
+
+
+
+// api router
+// import subdomain from 'express-subdomain'
+// import express from 'express'
+// import errHandle from 'rapid-error-handler'
+// const router = express.Router()
+//
+// // router imports
+// import v1 from './v1'
+//
+// // export function
+// module.exports = function() {
+//   return router
+//     // router middlewares
+//     .use(subdomain('v1', v1()))
+//
+//     // error handler
+//     .use(function(req, res, next) {
+//       next(new errHandle.Forbidden())
+//     })
+//     .use(function(err, req, res, next) {
+//       if(err && err.statusCode)
+//         res.status(err.statusCode).send({status: err.statusCode, message: err.message})
+//       else next(err)
+//     })
+// }
