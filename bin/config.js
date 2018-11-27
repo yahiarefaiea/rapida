4d8f9d0f36268a815cd875c4977cd35ea64147b3
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-11-27 20:49:22 +0200

    localtest.me >> localhost

--- bin/config.js
+++ bin/config.js
@@ -4,6 +4,6 @@ import pkg from '../package.json'
 module.exports = {
   project: pkg.name,
   env: process.env.NODE_ENV || 'development',
-  host: process.env.HOST || 'localtest.me',
+  host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3000
 }
