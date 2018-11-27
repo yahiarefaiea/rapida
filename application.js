dff781057dbd24d63e0db8212cdc4a0a33900c5d
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-11-27 15:52:06 +0200

    improve application.js

--- application.js
+++ application.js
@@ -1,10 +1,10 @@
 import config from './bin/config'
 import express from 'express'
+import morgan from 'morgan'
 import favicon from 'serve-favicon'
 import webpack from 'webpack'
 import webpackConfig from './webpack.config'
 
-// instance of express
 const app = express()
 const compiler = webpack(webpackConfig)
 
@@ -15,6 +15,7 @@ app.set('view engine', 'pug')
 // use middlewares
 app.use(express.static('storage'))
 app.use(favicon('storage/favicon.png'))
+app.use(morgan('dev'))
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: webpackConfig.output.publicPath
@@ -23,6 +24,13 @@ app.use(require('webpack-dev-middleware')(compiler, {
 // project middlewares
 app.get('*', function(req, res) {
   res.render('./ui/index')
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  if(err && err.statusCode)
+    res.status(err.statusCode).send({status: err.statusCode, message: err.message})
+  else next(err)
 })
 
 module.exports = app
