9b9473e8b17ad2ec4d7ed488167e548fb49e5985
Yahia Refaiea <yahiarefaiea@gmail.com>
2019-05-01 17:22:09 +0300

    match the ui structure

--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,5 @@
+import 'index.pug'
+
 const name = 'XXXX'
 
 // eslint-disable-next-line no-console
@@ -6,3 +8,23 @@ console.log(`Hello from the ${name} :(`)
 
 import library from './library'
 library()
+
+if(module.hot) module.hot.accept()
+
+if(module.hot) {
+  const hotEmitter = require('webpack/hot/emitter')
+  const DEAD_CSS_TIMEOUT = 2000
+
+  hotEmitter.on('webpackHotUpdate', function(currentHash) {
+    document.querySelectorAll('link[href][rel=stylesheet]').forEach(function(link) {
+      const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`)
+      const newLink = link.cloneNode()
+      newLink.href = nextStyleHref
+
+      link.parentNode.appendChild(newLink)
+      setTimeout(function() {
+        link.parentNode.removeChild(link)
+      }, DEAD_CSS_TIMEOUT)
+    })
+  })
+}
