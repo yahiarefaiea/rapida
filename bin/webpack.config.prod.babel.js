5cc5d6c099ffe9af0859ceee1f41d17b2fcc7661
Yahia Refaiea <yahiarefaiea@gmail.com>
2019-05-18 02:23:02 +0300

    clear webpack prod version from caching

--- bin/webpack.config.prod.babel.js
+++ bin/webpack.config.prod.babel.js
@@ -6,38 +6,16 @@ import baseConfig from './webpack.config.base'
 module.exports = merge(baseConfig, {
   devtool: 'source-map',
 
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
-        },
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
-  },
-
-  //  plugins
+  // plugins
   plugins: [
+    // banner plugin
     new webpack.BannerPlugin({
       banner: `${pkg.name} v${pkg.version}\n` +
               `${pkg.description}\n` +
-
-              //  add author somewhere..
-              `Last update on: ${new Date().getUTCFullYear()}/` +
-              `${new Date().getUTCMonth() + 1}/${new Date().getUTCDate()}\n` +
-              `Released under the ${pkg.license} license.\n` +
-              'hash:[hash], chunkhash:[chunkhash], file:[file]'
-    }),
-
-    new webpack.HashedModuleIdsPlugin()
+              `Project produced by: ${pkg.author}\n` +
+              `Last update on: ${new Date().getUTCFullYear()}.` +
+              `${new Date().getUTCMonth() + 1}.${new Date().getUTCDate()}\n` +
+              `Released under the ${pkg.license} license.`
+    })
   ]
 })
