6cc663925a39cee0ef5cdfbb0266ac128a104614
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-05-26 11:23:04 +0300

    minimize css and js

--- bin/webpack.config.prod.babel.js
+++ bin/webpack.config.prod.babel.js
@@ -2,9 +2,20 @@ import pkg from '../package.json'
 import webpack from 'webpack'
 import merge from 'webpack-merge'
 import baseConfig from './webpack.config.base'
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(baseConfig, {
-  devtool: 'source-map',
+  // optimization
+  optimization: {
+    minimizer: [
+      // TerserJSPlugin
+      new TerserJSPlugin({}),
+
+      // OptimizeCSSAssetsPlugin
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
 
   // plugins
   plugins: [
@@ -15,7 +26,9 @@ module.exports = merge(baseConfig, {
               `Project produced by: ${pkg.author}\n` +
               `Latest update on: ${new Date().getUTCFullYear()}.` +
               `${new Date().getUTCMonth() + 1}.${new Date().getUTCDate()}\n` +
-              `Released under the ${pkg.license} license.`
+              `Released under the ${pkg.license} license.\n` +
+              'hash: [hash], chunkhash: [chunkhash]\n' +
+              'file with contenthash: [file]'
     })
   ]
 })
