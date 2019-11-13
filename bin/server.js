f0067616769e970c13556ba5c4d165ab5532779b
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-11-13 16:33:00 +0200

    change cors() location in middlewares

--- bin/server.js
+++ bin/server.js
@@ -27,11 +27,11 @@ db.on('error', function() {
 })
 
 // use middlewares
+app.use(cors())
 app.use(compression())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors())
 
 // project middlewares
 app.use('/', api())
