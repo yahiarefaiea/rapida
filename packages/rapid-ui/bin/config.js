4aa099c181c7553a6d72cd7e512631b6f77d38c7
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-12-04 18:59:20 +0200

    fix conflicts with rapid-api and rapid-ui

--- packages/rapid-ui/bin/config.js
+++ packages/rapid-ui/bin/config.js
@@ -4,16 +4,8 @@ export default Object.freeze({
   project: pkg.name,
   env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8080,
   devMode: function() {
     if(this.env === 'development') return true
-  },
-  db: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 27017,
-    name: process.env.DB_NAME || pkg.name
-  },
-  database: function() {
-    return `mongodb://${this.db.host}:${this.db.port}/${this.db.name}`
   }
 })
