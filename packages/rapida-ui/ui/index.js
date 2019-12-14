645ed550e29c757ffe0070971cd546d172780277
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-12-14 19:17:46 +0200

    hands on fixing the HMR

--- packages/rapida-ui/ui/index.js
+++ packages/rapida-ui/ui/index.js
@@ -10,21 +10,4 @@ api.book.getAll()
   .catch(error => console.log(error))
 
 // enable hot module replacement plugin
-if(module.hot) {
-  module.hot.accept()
-  const hotEmitter = require('webpack/hot/emitter')
-  const DEAD_CSS_TIMEOUT = 2000
-
-  hotEmitter.on('webpackHotUpdate', function(currentHash) {
-    document.querySelectorAll('link[href][rel=stylesheet]').forEach(function(link) {
-      const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`)
-      const newLink = link.cloneNode()
-      newLink.href = nextStyleHref
-
-      link.parentNode.appendChild(newLink)
-      setTimeout(function() {
-        link.parentNode.removeChild(link)
-      }, DEAD_CSS_TIMEOUT)
-    })
-  })
-}
+if(module.hot) module.hot.accept()
