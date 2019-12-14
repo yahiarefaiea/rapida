b0a083b168493898c872d4e60805bcaf72157bd5
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-12-14 21:29:45 +0200

    remove unnecessary methods from AbstractController

--- packages/rapida-api/api/book/controller.js
+++ packages/rapida-api/api/book/controller.js
@@ -11,22 +11,6 @@ class Controller extends AbstractController {
     req.query = pick(req.query, 'read')
     super.getAll(req, res, next)
   }
-
-  post(req, res, next) {
-    super.post(req, res, next)
-  }
-
-  // get(req, res, next) {
-  //
-  // }
-
-  patch(req, res, next) {
-    super.patch(req, res, next)
-  }
-
-  // delete(req, res, next) {
-  //
-  // }
 }
 
 export default new Controller(Model)
