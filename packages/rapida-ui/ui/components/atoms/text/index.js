319d4b7f9b15fe07a027c9aef93896bd2cbc33ec
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2020-01-02 19:57:02 +0200

    log 'Rapida UI by Nuotron' in console inside #text atom

---
+++ packages/rapida-ui/ui/components/atoms/text/index.js
@@ -0,0 +1,4 @@
+export default (function() {
+  // eslint-disable-next-line no-console
+  console.log('Rapida UI by Nuotron')
+})()
--- packages/rapida-ui/ui/index.js
+++ packages/rapida-ui/ui/index.js
@@ -2,10 +2,9 @@ import './styles/index.styl'
 import lodash from 'lodash'
 import api from './api'
 import nuotron from './components/atoms/nuotron'
+import text from './components/atoms/text'
 
 /* eslint-disable no-console */
-console.log('Rapida UI')
-
 api.book.getAll()
   .then(data => console.log(data))
   .catch(error => console.log(error))
