eaf601db7ccaaaf29ef2a43fe95fc3fbf04f9998
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2020-01-02 19:53:24 +0200

    remove db variable and use mongoose.connection instead

--- packages/rapida-api/bin/server.js
+++ packages/rapida-api/bin/server.js
@@ -22,8 +22,7 @@ mongoose.connect(config.database(), {
   useCreateIndex: true,
   useUnifiedTopology: true
 })
-const db = mongoose.connection
-db.on('error', function() {
+mongoose.connection.on('error', function() {
   console.log(chalk.red(`Failed to connect to the \`${config.project}\` database`))
 }).once('open', function() {
   console.log(chalk.cyan(`Successfully connected to the \`${config.project}\` database`))
